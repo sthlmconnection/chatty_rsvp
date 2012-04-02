848e2a43ef88925bdb28b2c71505ef47c489097d
Hannes <hannes@sthlmconnection.se>
2012-04-02 19:32:55 +0200

    Better animation timing, plus fixes.

--- guest.js
+++ guest.js
@@ -36,9 +36,11 @@
   var changeState = function() {
     switch (step) {
       case 0:
-        $("#step-1").slideDown(500, function() {
-          $("#submit").val(texts.proceed).slideDown();
-        })
+        $("#step-1 .message").delay(1000).slideDown(200, function() {
+          $("#step-1 .input").delay(1000).slideDown(200, function() {
+            $("#submit").val(texts.proceed).fadeIn(100);
+          });
+        });
         step = 1;
         break;
 
@@ -58,19 +60,28 @@
                 $form.submit(); // No need to fill out step 2.
               }
             }
-            loading(false);
-            $("#step-2").slideDown();
+            $("#step-2 .message").slideDown(200, function() {
+              if (!nameKnown) {
+                $("#step-2 .input").delay(500).slideDown(200);
+              }
+              loading(false);
+            });
           });
         }
         break;
       case 2:
         if (values.email && values.name) {
+          loading(true);
           step = 3;
           if (!nameKnown) {
             $("#step-3 .message").text(texts.step_3.message_new
               .replace(/%name/, firstName(values.name)));
           }
-          $("#step-3").slideDown();
+          $("#step-3 .message").delay(nameKnown ? 1000 : 500).slideDown(200, function() {
+            $("#step-3 .input").delay(500).slideDown(200, function() {
+              loading(false);
+            });
+          });
         }
         else {
           alert(texts.input_error);
@@ -78,22 +89,29 @@
         break;
       case 3:
         if (values.email && values.name) {
+          loading(true);
           step = 4;
           switch (values.coming) {
             case "0":
               $("#step-4 .message").text(texts.step_4.message_coming_no);
-              $("#step-4 .input").hide();
+              $("#step-5 .message:eq(0)").text(texts.step_5.message_coming_no);
+              $("#step-4 .input").attr("data-hide", "true");
               $form.submit(); // No need to fill out step 4.
               break;
             case "0.5":
               $("#step-4 .message").text(texts.step_4.message_coming_maybe);
+              $("#step-5 .message:eq(0)").text(texts.step_5.message_coming_maybe);
               break;
             case "1":
               $("#step-4 .message").text(texts.step_4.message_coming_yes);
+              $("#step-5 .message:eq(0)").text(texts.step_5.message_coming_yes);
               break;
           }
           $("submit", $form).val(texts.submit);
-          $("#step-4").slideDown();
+          $("#step-4 .message").delay(500).slideDown(200, function() {
+            $("#step-4 .input:not([data-hide=true])").delay(500).slideDown(200);
+            loading(false);
+          });
         }
         else {
           alert(texts.input_error);
@@ -107,10 +125,13 @@
           }
           else {
             step = 5;
-            $("#submit", $form).slideUp();
-            $("#step-5").slideDown();
+            $("#submit", $form).fadeOut(100, function() {
+              loading(false);
+              $("#step-5 .message:eq(0)").slideDown(200, function() {
+                $("#step-5 .message:eq(1)").delay(500).slideDown(200);
+              });
+            });
           }
-          loading(false);
         });
         break;
     }
