d64d9b324052826a9fb133271b12076f785e1432
Hannes <hannes@sthlmconnection.se>
2012-04-02 18:33:49 +0200

    Better styling.

--- index.php
+++ index.php
@@ -18,38 +18,42 @@ $css_file = file_exists('style.css') ? 'style.css' : 'default.style.css';
     <script src="guest.js"></script>
   </head>
   <body>
-    <?php print $texts->intro; ?>
-    <form id="rsvp" action="guest.php" method="post">
-      <div id="step-1" class="step">
-        <p class="message"><?php print $texts->step_1; ?></p>
-        <div class="input">
-          <input type="email" name="email" id="email" value="" />
-        </input>
-      </div>
-      <div id="step-2" class="step">
-        <p class="message"><?php print $texts->step_2->message_new; ?></p>
-        <input type="text" name="name" id="name" value="" />
-      </div>
-      <div id="step-3" class="step">
-        <p class="message"><?php print $texts->step_3->message_existing; ?></p>
-        <div class="input">
-          <input type="radio" name="coming" value="1" checked="checked"> <?php print $texts->step_3->option_yes; ?>
-          <input type="radio" name="coming" value="0"> <?php print $texts->step_3->option_no; ?>
-          <input type="radio" name="coming" value="0.5"> <?php print $texts->step_3->option_maybe; ?>
+    <div class="page">
+      <?php print $texts->intro; ?>
+      <form id="rsvp" action="guest.php" method="post">
+        <div id="step-1" class="step">
+          <p class="message"><?php print $texts->step_1; ?></p>
+          <div class="input">
+            <input type="email" name="email" id="email" value="" />
+          </input>
         </div>
-      </div>
-      <div id="step-4" class="step">
-        <p class="message"><?php print $texts->step_4->message_default; ?></p>
-        <div class="input">
-          <input type="radio" name="friend" value="1"> <?php print $texts->step_4->option_yes; ?>
-          <input type="radio" name="friend" value="0" checked="checked"> <?php print $texts->step_4->option_no; ?>
+        <div id="step-2" class="step">
+          <p class="message"><?php print $texts->step_2->message_new; ?></p>
+          <div class="input">
+            <input type="text" name="name" id="name" value="" />
+          </div>
         </div>
-      </div>
-      <input type="submit" name="submit" id="submit" value="<?php print $texts->submit; ?>" />
-      <div id="step-5" class="step">
-        <p class="message"><?php print $texts->step_5->message_default; ?></p>
-        <p class="message"><?php print $texts->bye; ?></p>
-      </div>
-    </form>
+        <div id="step-3" class="step">
+          <p class="message"><?php print $texts->step_3->message_existing; ?></p>
+          <div class="input">
+            <input type="radio" name="coming" value="1" checked="checked"> <?php print $texts->step_3->option_yes; ?>
+            <input type="radio" name="coming" value="0"> <?php print $texts->step_3->option_no; ?>
+            <input type="radio" name="coming" value="0.5"> <?php print $texts->step_3->option_maybe; ?>
+          </div>
+        </div>
+        <div id="step-4" class="step">
+          <p class="message"><?php print $texts->step_4->message_default; ?></p>
+          <div class="input">
+            <input type="radio" name="friend" value="1"> <?php print $texts->step_4->option_yes; ?>
+            <input type="radio" name="friend" value="0" checked="checked"> <?php print $texts->step_4->option_no; ?>
+          </div>
+        </div>
+        <input type="submit" name="submit" id="submit" value="<?php print $texts->submit; ?>" />
+        <div id="step-5" class="step">
+          <p class="message"><?php print $texts->step_5->message_default; ?></p>
+          <p class="message"><?php print $texts->bye; ?></p>
+        </div>
+      </form>
+    </div>
   </body>
 <html>
