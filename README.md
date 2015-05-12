3837a8776f3c7750f9c01ac2d0e5745bbda92c93
Hannes Lilljequist <hannes@sthlmconnection.se>
2015-05-12 15:45:21 +0200

    Made the markup customizable.

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Chatty RSVP page
 
-This is a small PHP and JavaScript application that provides a nifty little RSVP page with a chat-style interface. The idea is to make the guests feel welcome and give a personal touch to an otherwise rather boring process.
+This is a small PHP and JavaScript application that provides a nifty little RSV
+page with a chat-style interface. The idea is to make the guests feel welcome
+and give a personal touch to an otherwise rather boring process.
 
 ## Features
 
 * GoogleDocs integration – saves RSVPs to a spreadsheet.
 * Can deal with and update existing guest records as well as adding new guests.
 * Friendly, interactive, chat-like interface.
-* Intelligently asks the right questions and talks to the user based on existing information.
+* Intelligently asks the right questions and talks to the user based on existing
+information.
 
 ## Unfeatures
 
@@ -19,24 +22,33 @@ This is a small PHP and JavaScript application that provides a nifty little RSVP
 
 * PHP (probably 5.2 minimum).
 * A Gmail/Google Apps account.
-* [Zend Framework](http://framework.zend.com/) – specifically the GData extension.
+* [Zend Framework](http://framework.zend.com/) – specifically the GData
+  extension.
 
 ## Getting started
 
 * Download the project files.
-* Download [Zend Framework](http://framework.zend.com/download/current/). The Minimal package is sufficient. Put the `Zend` directory directly under this directory.
-* Create a GoogleDocs spreadsheet with the following columns, in this order (important):
+* Download [Zend Framework](http://framework.zend.com/download/current/). The
+  Minimal package is sufficient. Put the `Zend` directory directly under this
+  directory.
+* Create a GoogleDocs spreadsheet with the following columns, in this order
+  (important):
   * Email
   * FirstName
   * LastName
   * Coming
   * Friend
   * Message
-* Set up a new Gmail/Google Apps account with the least permissions possible while still being able to edit the spreadsheet.
+* Set up a new Gmail/Google Apps account with the least permissions possible
+  while still being able to edit the spreadsheet.
 * Create a subdirectory called `custom`.
-* Make a copy of `default/config.php` in the `custom` directory. Fill in the required options. Keep it secret. Keep it safe.
+* Make a copy of `default/config.php` in the `custom` directory. Fill in the
+  required options. Keep it secret. Keep it safe.
 
 ## Customization
 
 * Make a copy of `default/style.css` at `custom/style.css`. Go crazy.
-* Make a copy of `default/texts.json` in the `custom` directory and add your personal language to it.
+* Make a copy of `default/texts.json` in the `custom` directory and add your
+  personal language to it.
+* Make a copy of `default/template.php` in the `custom` directory if you want to
+  tweak the markup.
---
+++ default/template.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html>
+  <head>
+    <title><?php print $texts->title; ?></title>
+    <meta charset="utf-8" />
+    <style>@import url(<?php print $css_file; ?>);</style>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script>jQuery("html").addClass("js");</script>
+    <script>var texts = <?php print $texts_json; ?>;</script>
+    <script src="chatty_rsvp.js"></script>
+  </head>
+  <body>
+    <div class="page">
+      <div class="intro">
+        <?php print $texts->intro; ?>
+      </div>
+      <form id="rsvp" action="guest.php" method="post" autocomplete="off">
+        <div id="step-1" class="step">
+          <p class="message"><?php print $texts->step_1; ?></p>
+          <div class="input">
+            <input type="email" name="email" id="email" value="" />
+          </div>
+        </div>
+        <div id="step-2" class="step">
+          <p class="message"><?php print $texts->step_2->message_new; ?></p>
+          <div class="input">
+            <input type="text" name="nameinput" id="nameinput" value="" />
+            <input type="hidden" name="firstname" id="firstname" value="" />
+            <input type="hidden" name="lastname" id="lastname" value="" />
+          </div>
+        </div>
+        <div id="step-3" class="step">
+          <p class="message"><?php print $texts->step_3->message_existing; ?></p>
+          <div class="input">
+            <label for="radio-1"><input id="radio-1" type="radio" name="coming" value="1" checked="checked"> <?php print $texts->step_3->option_yes; ?></label>
+            <label for="radio-2"><input id="radio-2" type="radio" name="coming" value="0"> <?php print $texts->step_3->option_no; ?></label>
+            <label for="radio-3"><input id="radio-3" type="radio" name="coming" value="0.5"> <?php print $texts->step_3->option_maybe; ?></label>
+          </div>
+        </div>
+        <div id="step-4" class="step">
+          <p class="message"><?php print $texts->step_4->message_default; ?></p>
+          <div class="input">
+            <label for="radio-2-1"><input id="radio-2-1" type="radio" name="friend" value="1"> <?php print $texts->step_4->option_yes; ?></label>
+            <label for="radio-2-2"><input id="radio-2-2" type="radio" name="friend" value="0" checked="checked"> <?php print $texts->step_4->option_no; ?></label>
+          </div>
+        </div>
+        <div id="step-5" class="step">
+          <p class="message"><?php print $texts->step_5->message; ?></p>
+          <div class="input">
+            <input type="text" name="message" id="message" value="">
+          </div>
+        </div>
+        <input type="submit" name="submit" id="submit" value="<?php print $texts->submit; ?>" />
+        <div id="step-6" class="step">
+          <p class="message"><?php print $texts->step_6->message_default; ?></p>
+          <p class="message"><?php print $texts->bye; ?></p>
+        </div>
+      </form>
+    </div>
+  </body>
+<html>
